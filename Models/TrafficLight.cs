1bb4d89c84bf180d20adde99b86bda2aea881468
luiarhs <luiarhs@yandex.com>
2024-05-03 22:37:12 -0600

    [add] model

---
+++ Models/TrafficLight.cs
@@ -0,0 +1,21 @@
+namespace JourneyAPI.Models
+{
+    public enum LightState
+    {
+        Green,
+        Yellow,
+        Red
+    }
+
+    public class TrafficLight
+    {
+        public string Direction { get; set; } = string.Empty;
+        public LightState CurrentState { get; set; }
+        public string CurrentStateColor => CurrentState.ToString();
+        public int GreenDuration { get; set; }
+        public int YellowDuration { get; set; } = 5;
+        public DateTime LastTransitionTime { get; set; }  // The last state transition
+        public bool IsRightTurnActive { get; set; } = false;  // Check the right-turn signal
+        public int GroupId { get; set; }  // 1 for North-South, 2 for East-West
+    }
+}
